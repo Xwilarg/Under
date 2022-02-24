ff95393d6bd4db5b6b4d9e4488466bde92a837fb
Xwilarg <xwilarg@yahoo.fr>
2022-02-24 23:35:31 +0100

    Fix maze generation

--- Assets/Scripts/Map/MapGeneration.cs
+++ Assets/Scripts/Map/MapGeneration.cs
@@ -42,7 +42,7 @@ namespace VarVarGamejam.Map
                 }
             }
 
-            List<Vector2Int> pending = new();
+            List<PosDir> pending = new();
             List<Vector2Int> allDirs = new()
             {
                 Vector2Int.left,
@@ -55,7 +55,11 @@ namespace VarVarGamejam.Map
             var mid = (_info.MapSize - 1) / 2 + 1;
             foreach (var dir in allDirs)
             {
-                pending.Add(new Vector2Int(mid + dir.y * 2, mid + dir.x * 2));
+                pending.Add(new()
+                {
+                    Pos = new Vector2Int(mid + dir.x * 2, mid + dir.y * 2),
+                    Dir = -dir
+                });
             }
             _map[mid][mid] = TileType.Entrance;
 
@@ -67,39 +71,48 @@ namespace VarVarGamejam.Map
                 var rand = pending[pIndex];
                 pending.RemoveAt(pIndex);
 
-                _map[rand.y][rand.x] = TileType.Empty; // Current room
+                _map[rand.Pos.y][rand.Pos.x] = TileType.Empty; // Current room
 
-                List<Vector2Int> availableDirs = new(allDirs);
-                // Remove positions that doesn't lead to an empty space
-                // TODO: keep track of previous pos to avoid loops in maze
-                availableDirs.RemoveAll(x =>
-                {
-                    var px = rand.x + x.x * 2;
-                    var py = rand.y + x.y * 2;
-                    if (IsOutOfBounds(py, px, _info.MapSize))
-                    {
-                        return true;
-                    }
-                    return _map[py][px] != TileType.Empty && _map[py][px] != TileType.Entrance;
-                });
-                var dir = availableDirs[Random.Range(0, availableDirs.Count)];
-                var pos = rand + dir * 2;
-                _map[rand.y + dir.y][rand.x + dir.x] = TileType.Empty; // Corridor
+                _map[rand.Pos.y + rand.Dir.y][rand.Pos.x + rand.Dir.x] = TileType.Empty; // Corridor
                 foreach (var d in allDirs)
                 {
-                    var newY = rand.y + d.y * 2;
-                    var newX = rand.x + d.x * 2;
+                    var newY = rand.Pos.y + d.y * 2;
+                    var newX = rand.Pos.x + d.x * 2;
 
                     // Check if position is valid
                     if (IsOutOfBounds(newY, newX, _info.MapSize))
                     {
-                        //Debug.Log("Invalid pos");
                         continue;
                     }
 
                     if (_map[newY][newX] == TileType.Pending)
                     {
-                        pending.Add(new(newX, newY));
+                        var pPos = new Vector2Int(newX, newY);
+                        var curr = pending.FirstOrDefault(x => x.Pos == pPos);
+                        if (curr == null)
+                        {
+                            pending.Add(new()
+                            {
+                                Pos = new(newX, newY),
+                                Dir = -d
+                            });
+                        }
+                        else
+                        {
+                            curr.Dir = -d;
+                        }
+                    }
+                }
+            }
+
+            // Once we are done, we replace unused "corridors" by walls
+            for (int y = 0; y < _info.MapSize; y++)
+            {
+                for (int x = 0; x < _info.MapSize; x++)
+                {
+                    if (_map[y][x] == TileType.Pending)
+                    {
+                        _map[y][x] = TileType.Wall;
                     }
                 }
             }
@@ -108,6 +121,12 @@ namespace VarVarGamejam.Map
         private bool IsOutOfBounds(int y, int x, int size)
         {
             return y <= 0 || x <= 0 || y >= size - 1 || x >= size - 1;
+        }
+
+        private record PosDir
+        {
+            public Vector2Int Pos;
+            public Vector2Int Dir;
         }
 
         private enum Direction
