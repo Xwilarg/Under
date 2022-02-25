524df5e34b2861f432585bb2c25256fb4acf0dec
Xwilarg <xwilarg@yahoo.fr>
2022-02-25 23:46:02 +0100

    Add translation system

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VarVarGamejam.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
---
+++ Assets/Scripts/Translation/TMP_TextTranslate.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace VarVarGamejam.Translation
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TMP_TextTranslate : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<TMP_Text>().text = Translate.Instance.Tr(GetComponent<TMP_Text>().text);
+        }
+    }
+}
