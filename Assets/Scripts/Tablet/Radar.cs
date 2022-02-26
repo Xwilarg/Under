6e6a69b80d06ef563d914d8af40a2789f8db4c3b
Xwilarg <xwilarg@yahoo.fr>
2022-02-26 14:21:34 +0100

    Add radar display

---
+++ Assets/Scripts/Tablet/Radar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace VarVarGamejam.Tablet
+{
+    public class Radar : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _line;
+
+        [SerializeField]
+        private float _speed;
+
+        private void Update()
+        {
+            _line.transform.Rotate(0f, 0f, Time.deltaTime * _speed);
+        }
+    }
+}
